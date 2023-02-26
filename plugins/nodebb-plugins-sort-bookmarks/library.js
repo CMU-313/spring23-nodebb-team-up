267dc8af588a01fe56e6f0e2cc2384287a030915
Amita <amitag@andrew.cmu.edu>
2023-02-25 22:34:35 -0500

    wrote sorting method for plugin library

--- plugins/nodebb-plugins-sort-bookmarks/library.js
+++ plugins/nodebb-plugins-sort-bookmarks/library.js
@@ -3,24 +3,19 @@
 var async = require('async');
 var Topics = require.main.require('./src/topics');
 
-var Plugin = {};
-
-Plugin.sortByBookmarks = function(data, callback) {
-  async.waterfall([
-    async.apply(Topics.getTopicsFromSet, 'topics:tid', 0, -1, 'topics:bookmark_count'),
+var Plugin = {sortByBookmarks :
     function(result, next) {
-      var tids = result.topics.map(function(topic) {
-        return topic.tid;
-      });
-      Topics.getTopicsByTids(tids, next);
-    },
-    function(topics, next) {
-      topics.sort(function(a, b) {
-        return b.bookmarks - a.bookmarks;
-      });
-      next(null, {topics: topics});
+        var tids = result.topics.map(function(topic) {
+            return topic.tid;
+        });
+        Topics.getTopicsByTids(tids, next);
+        },
+        function(topics, next) {
+        topics.sort(function(a, b) {
+            return b.bookmarks - a.bookmarks;
+        });
+        next(null, {topics: topics});
     }
-  ], callback);
 };
 
 module.exports = Plugin;

f8653e2c254d03d10c3d482d397a47773312bceb
Amita <amitag@andrew.cmu.edu>
2023-02-26 19:03:53 -0500

    added sort method and plugin definition for bookmark sorting feature

--- plugins/nodebb-plugins-sort-bookmarks/index.js
+++ plugins/nodebb-plugins-sort-bookmarks/index.js
@@ -1,5 +1,28 @@
 'use strict';
 
-const Topics = require.main.require('./src/topics');
-const bookmarks = require.main.require('./src/bookmarks');
+// Referenced NodeJS documentation from https://nodejs.org/api/modules.html#requiremain
+const Topics = module.children.require('./src/topics');
+const bookmarks = module.children.require('./src/bookmarks');
 
+const sort = async function (req) {
+    const topicsList = await Topics.getTopicData(req.data.tids);
+    for (const topic of topicsList) {
+        topic.bookmarkCount = bookmarks.count(`topic:${topic.tid}:bookmarks`);
+    }
+    topicsList.sort((a, b) => b.bookmarkCount - a.bookmarkCount);
+    console.log(topicsList);
+    return topicsList;
+};
+
+// Referenced Plugin documentatino from https://docs.nodebb.org/development/plugins/
+
+const Plugin = {
+    displaySortedTopics: async function (data) {
+        if (data.req.query.sort === 'bookmarks') {
+            data.results = await sort(data.req);
+        }
+        return data;
+    },
+};
+
+module.exports = Plugin;

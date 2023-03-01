3b04fa7f89eef0ef8ff61fa2cd7ef3e4acf6faee
Amita <amitag@andrew.cmu.edu>
2023-02-28 22:03:13 -0500

    wrote tests for sort plugin

--- plugins/nodebb-plugin-sort-bookmarks/test/sort-bookmarks-test.js
+++ plugins/nodebb-plugin-sort-bookmarks/test/sort-bookmarks-test.js
@@ -3,4 +3,36 @@
 const assert = require('assert');
 const Plugin = require('../index');
 const Topics = require('../../../src/topics');
-const bookmarks = module.children.require('./src/bookmarks');
+const bookmarks = require('../../../src/bookmarks');
+
+describe(' test Plugin', function () {
+    describe('Plugin.displaySortedTopics', function () {
+        //create sample TopicData output to test sort method 
+        const req = {
+            data: {tids: [1, 2, 3]},
+            uid: 1
+        };
+        const results = {
+            topics: [
+            { tid: 1, bookmarks: 3 },
+            { tid: 2, bookmarks: 1 },
+            { tid: 3, bookmarks: 2 },
+            ],
+        };
+        Topics.getTopicData = async function () {
+            return results;
+        };
+
+  
+      it('should sort topics by the number of bookmarks', async function () {
+          const data = await Plugin.displaySortedTopics({req});
+          assert.equal(data.results, results);
+          assert.equal(data.results.topics, [
+            { tid: 1, bookmarkCount: 3 },
+            { tid: 3, bookmarkCount: 2 },
+            { tid: 2, bookmarkCount: 1 },
+          ]);
+        });
+    });
+  });
+  

0643e7ccbe142189d43ad8798b6e273b67258f95
Amita <amitag@andrew.cmu.edu>
2023-02-28 22:08:13 -0500

    removed bookmark imports from plugin

--- plugins/nodebb-plugin-sort-bookmarks/index.js
+++ plugins/nodebb-plugin-sort-bookmarks/index.js
@@ -2,25 +2,17 @@
 
 // Referenced NodeJS documentation from https://nodejs.org/api/modules.html#requiremain
 const Topics = module.children.require('./src/topics');
-const bookmarks = module.children.require('./src/bookmarks');
 
 const sort = async function (req) {
     const topicsList = await Topics.getTopicData(req.data.tids);
-    for (const topic of topicsList) {
-        topic.bookmarkCount = bookmarks.count(`topic:${topic.tid}:bookmarks`);
-    }
-    topicsList.sort((a, b) => b.bookmarkCount - a.bookmarkCount);
-    console.log(topicsList);
+    topicsList.sort((a, b) => b.bookmarks - a.bookmarks);
     return topicsList;
 };
 
-// Referenced Plugin documentatino from https://docs.nodebb.org/development/plugins/
-
+// Referenced Plugin documentation from https://docs.nodebb.org/development/plugins/
 const Plugin = {
     displaySortedTopics: async function (data) {
-        if (data.req.query.sort === 'bookmarks') {
-            data.results = await sort(data.req);
-        }
+        data.results = await sort(data.req);
         return data;
     },
 };

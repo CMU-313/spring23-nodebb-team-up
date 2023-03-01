3732743eb65e4adede5bc2a0f1751fd29e26b1cb
Amita <amitag@andrew.cmu.edu>
2023-02-28 21:55:27 -0500

    created test file for sort bookmarks

---
+++ plugins/nodebb-plugin-sort-bookmarks/test/sort-bookmarks-test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const assert = require('assert');
+const Plugin = require('../index');
+const Topics = require('../../../src/topics');
+const bookmarks = module.children.require('./src/bookmarks');

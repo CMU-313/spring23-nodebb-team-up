b4da53c5bd8159ab2ed12115bb83f6ded2a3c044
ala <ala@aladembp.mynetworksettings.com>
2023-03-16 21:21:27 -0400

    added iroh tool

---
+++ iroh/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Iroh = require('iroh');
+
+let code = `let foo = 42`;
+
+let stage = new Iroh.Stage(code);
+stage.addListener(Iroh.VAR).on("after", function(e) {
+  console.log(e.name, "=>", e.value);
+});
+
+eval(stage.script);
